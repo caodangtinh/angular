9e3b4df7e8026038a2eb2a4bf2ecd99e04f7b915
tinh cao <caodangtinh@gmail.com>
2018-12-25 11:58:10 +0700

    add node and node-row component

--- angular-in-action/datacenter/src/app/nodes-row/nodes-row.component.ts
+++ angular-in-action/datacenter/src/app/nodes-row/nodes-row.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Node } from '../model/node';
 
 @Component({
-  selector: 'app-nodes-row',
+  selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
   styleUrls: ['./nodes-row.component.css']
 })
 export class NodesRowComponent implements OnInit {
 
+  @Input('node') node: Node;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isDanger(prop: string): boolean {
+    return this.node[prop].used / this.node[prop].available > 0.7;
   }
 
 }
--- angular-in-action/datacenter/src/app/nodes/nodes.component.ts
+++ angular-in-action/datacenter/src/app/nodes/nodes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nodes',
+  selector: '[app-nodes]',
   templateUrl: './nodes.component.html',
   styleUrls: ['./nodes.component.css']
 })
@@ -10,6 +10,7 @@ export class NodesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
