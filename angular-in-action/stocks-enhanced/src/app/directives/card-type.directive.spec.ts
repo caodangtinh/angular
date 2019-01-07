635b19dd3cbc72cdd45e660422726f879c717cab
tinhcao <caodangtinh@gmail.com>
2019-01-07 22:40:27 +0700

    remove test file

--- angular-in-action/stocks-enhanced/src/app/directives/card-type.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CardTypeDirective } from './card-type.directive';
-
-describe('CardTypeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CardTypeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
